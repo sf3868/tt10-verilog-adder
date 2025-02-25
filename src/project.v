b7affb14bc2412ef4a7c704dd2a14fb723adb51b
Brian <138974256+brian-md-12@users.noreply.github.com>
2025-02-24 23:54:43 -0500

    Update project.v
    This is an updated code based on question 2. I have used concatenation in the mean time.

--- src/project.v
+++ src/project.v
@@ -5,12 +5,12 @@
 
 `default_nettype none
 
-module tt_um_project (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+module priority_16_8_encoder (
+    input  wire [15:0] ui_in,    // Dedicated inputs
+    output reg [7:0] uo_out,   // Dedicated outputs
+    input  wire [15:0] uio_in,   // IOs: Input path
+    output reg [7:0] uio_out,  // IOs: Output path
+    output reg [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -24,4 +24,30 @@ module tt_um_project (
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+    always @(*) begin
+        // Concatenate A and B into In
+        In = {ui_in, uio_in};
+        // Default output for the special case (all inputs are 0)
+        C = 8'b11110000;
+
+        // Priority encoding logic
+        if (In[15]) C = 8'd15;
+        else if (In[14]) C = 8'd14;
+        else if (In[13]) C = 8'd13;
+        else if (In[12]) C = 8'd12;
+        else if (In[11]) C = 8'd11;
+        else if (In[10]) C = 8'd10;
+        else if (In[9]) C = 8'd9;
+        else if (In[8]) C = 8'd8;
+        else if (In[7]) C = 8'd7;
+        else if (In[6]) C = 8'd6;
+        else if (In[5]) C = 8'd5;
+        else if (In[4]) C = 8'd4;
+        else if (In[3]) C = 8'd3;
+        else if (In[2]) C = 8'd2;
+        else if (In[1]) C = 8'd1;
+        else if (In[0]) C = 8'd0;
+    end
+
 endmodule
+
