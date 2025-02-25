fe9ddccb73e401193718b41816433ce72f5e30cc
Brian <138974256+brian-md-12@users.noreply.github.com>
2025-02-24 23:58:10 -0500

    Update tb.v
    testing tb

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_project user_project (
+  priority_16_8_encoder (
      
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
